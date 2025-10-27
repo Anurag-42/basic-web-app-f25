44e45c82ecbabce918142a500457837f0c0b58c8
Anurag Aryal <aryalanurag20602003@gmail.com>
2025-10-27 12:32:51 +0300

    goated

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -72,5 +72,41 @@ if (sqCubeMatch) {
   return result.join(", ");
 }
 
+const scrabbleMatch = query.toLowerCase().match(/what is the scrabble score of (\w+)\?/);
+if (scrabbleMatch) {
+  const word = scrabbleMatch[1].toUpperCase();
+  const scores: Record<string, number> = {
+    A:1, B:3, C:3, D:2, E:1, F:4, G:2, H:4,
+    I:1, J:8, K:5, L:1, M:3, N:1, O:1, P:3,
+    Q:10, R:1, S:1, T:1, U:1, V:4, W:4, X:8, Y:4, Z:10
+  };
+  let total = 0;
+  for (const char of word) {
+    total += scores[char] || 0;
+  }
+  return total.toString();
+}
+
+const arithmeticExprMatch = query.toLowerCase().match(/what is (.+)\?/);
+if (arithmeticExprMatch) {
+  let expr = arithmeticExprMatch[1];
+
+  // Replace words with symbols
+  expr = expr
+    .replace(/plus/g, '+')
+    .replace(/minus/g, '-')
+    .replace(/multiplied by|times/g, '*')
+    .replace(/divided by/g, '/');
+
+  try {
+    // Evaluate expression
+    const result = eval(expr);
+    return result.toString();
+  } catch {
+    return "";
+  }
+}
+
+
   return "";
 }
